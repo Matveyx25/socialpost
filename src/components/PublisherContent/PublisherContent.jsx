b4b0a4a16c3ca527b648e2587ad68bbbee4f1940
Matveyx25 <matveyx25@gmail.com>
2023-07-06 22:57:07 +0400

    First try scroll animation

--- src/components/PublisherContent/PublisherContent.jsx
+++ src/components/PublisherContent/PublisherContent.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import s from './PublisherContent.module.scss'
+import { Path } from '../Path/Path';
 
 export const PublisherContent = () => {
+	let start = useRef(null)
+
 	return (
-		<div className={s.wrapper}>
-			
-		</div>
-	)
+    <div className={s.wrapper}>
+			<div className="container">
+				<Path {...{start}}/>
+				<div className={s.flex}>
+					<div className={s.info} ref={ref => start = ref}>
+						<img src="/images/publisher/img1.svg" alt="" className='motion-section'/>
+						Легализация размещения<br/>
+						постов с автоматической<br/>
+						передачей данных в ОРД
+					</div>
+					<div className={s.info}>
+					<img src="/images/publisher/img2.svg" alt="" />
+						Работа в соответствии с законом о<br/>
+						рекламе - регистрируем а ОРД все<br/>
+						необходимы данные за Вас
+					</div>
+					<div className={s.info}>
+						<img src="/images/publisher/img3.svg" alt="" />
+						Выплаты любым удобным<br/>
+						Вам способом
+					</div>
+				</div>
+			</div>
+    </div>
+  );
 }
