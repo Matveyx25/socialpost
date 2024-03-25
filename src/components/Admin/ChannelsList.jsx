d02b8de453b2b09b7432f804159104662d24169f
Matveyx25 <matveyx25@gmail.com>
2024-03-25 17:51:45 +0400

    Confirmed in admin

--- src/components/Admin/ChannelsList.jsx
+++ src/components/Admin/ChannelsList.jsx
@@ -1,20 +1,17 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ImageField, BooleanField, SingleFieldList, ReferenceArrayField } from "react-admin";
+import { List, Datagrid, TextField, ImageField, BooleanField, SingleFieldList, ReferenceArrayField, FunctionField } from "react-admin";
 
 export const ChannelsList = (props) => (
   <List {...props}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
-      <TextField source="name" label="Имя"/>
-      <TextField source="telegramUsername" label="username"/>
-      <TextField source="telegramId" label="id"/>
+      <TextField source="name" label="Название"/>
       <TextField source="subscribersCount" label="Подписчики"/>
       <TextField source="engagementRate" label="ER"/>
       <TextField source="costPerView" label="CPV"/>
-      <TextField source="averagePostReach" label="Средний охват"/>
-      <TextField source="averagePostReach" label="Средний охват"/>
       <ImageField source="imageUrl" sx={{ '& img': { maxWidth: 50, maxHeight: 50, objectFit: 'contain' } }} label="Фото"/>
-      <BooleanField source="status" valueLabelFalse="NOT_CONFIRMED" valueLabelTrue="CONFIRMED" label="Подтвержден"/>
+      <FunctionField source="status" label="Подтверждние" render={(record,source) => 
+    		<BooleanField record={{...record, status: source === 'CONFIRMED'}} source={source}/>}/>
 			<ReferenceArrayField label="Тэги" reference="tag" source="tag">
 					<SingleFieldList />
 			</ReferenceArrayField>
