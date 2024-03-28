6c91badfc31dbd0891227edf2049478b6f4c7e18
Matveyx25 <matveyx25@gmail.com>
2024-03-29 01:10:24 +0400

    Roles

--- src/components/Admin/UserList.jsx
+++ src/components/Admin/UserList.jsx
@@ -1,6 +1,32 @@
 import { Chip } from "@mui/material";
 import * as React from "react";
-import { List, Datagrid, TextField, EmailField, ImageField, BooleanField, SingleFieldList, ChipField } from "react-admin";
+import { List, Datagrid, TextField, EmailField, ImageField, BooleanField, SingleFieldList, ReferenceArrayField, ArrayField, ChipField } from "react-admin";
+
+const RolesArrayField = ({ record, source }) => {
+	const roles = record[source];
+	if (!roles || roles.length === 0) {
+			return <div/>;
+	}
+	return (
+			<>
+					{roles.map(role => <Chip label={role} key={role}/>)}
+			</>
+	);
+};
+
+export const UserList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="firstName" label="Имя"/>
+      <TextField source="lastName" label="Фамилия"/>
+      <ImageField source="photoUrl" sx={{ '& img': { maxWidth: 50, maxHeight: 50, objectFit: 'contain' } }} label="Фото"/>
+      <EmailField source="emailData.email" label="Эл. почта"/>
+      <BooleanField source="telegramData" valueLabelFalse="null" valueLabelTrue="!!telegramData" label="Телеграм"/>
+			<RolesArrayField source="roles" label="Роли"/>
+    </Datagrid>
+  </List>
+);
 
 const TextArrayField = ({ record, source }) => {
   const array = record[source]
@@ -15,21 +41,3 @@ const TextArrayField = ({ record, source }) => {
   }
 }
 TextArrayField.defaultProps = { addLabel: true }
-
-export const UserList = (props) => (
-  <List {...props}>
-    <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="firstName" label="Имя"/>
-      <TextField source="lastName" label="Фамилия"/>
-      <ImageField source="photoUrl" sx={{ '& img': { maxWidth: 50, maxHeight: 50, objectFit: 'contain' } }} label="Фото"/>
-      <EmailField source="emailData.email" label="Эл. почта"/>
-      <BooleanField source="telegramData" valueLabelFalse="null" valueLabelTrue="!!telegramData" label="Телеграм"/>
-			<TextArrayField source="roles" label="Роли">
-				<SingleFieldList>
-					<ChipField source="id" />
-				</SingleFieldList>
-			</TextArrayField>
-    </Datagrid>
-  </List>
-);
