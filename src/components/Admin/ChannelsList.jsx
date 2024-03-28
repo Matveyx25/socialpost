242d2012b47f3758f1764cca098edb2232b82ad9
Matveyx25 <matveyx25@gmail.com>
2024-03-28 14:56:56 +0400

    Status in channel list

--- src/components/Admin/ChannelsList.jsx
+++ src/components/Admin/ChannelsList.jsx
@@ -10,8 +10,7 @@ export const ChannelsList = (props) => (
       <TextField source="engagementRate" label="ER"/>
       <TextField source="costPerView" label="CPV"/>
       <ImageField source="imageUrl" sx={{ '& img': { maxWidth: 50, maxHeight: 50, objectFit: 'contain' } }} label="Фото"/>
-      <FunctionField source="status" label="Подтверждние" render={(record,source) => 
-    		<BooleanField record={{...record, status: source === 'CONFIRMED'}} source={source}/>}/>
+    	<BooleanField source='status' valueLabelTrue="CONFIRMED"/>
 			<ReferenceArrayField label="Тэги" reference="tag" source="tag">
 					<SingleFieldList />
 			</ReferenceArrayField>
