db0cc3497a1a307e888a16cf994827bc3586d46e
Matveyx25 <matveyx25@gmail.com>
2024-03-29 21:58:17 +0400

    roles chip

--- src/components/Admin/UserList.jsx
+++ src/components/Admin/UserList.jsx
@@ -2,18 +2,6 @@ import { Chip } from "@mui/material";
 import * as React from "react";
 import { List, Datagrid, TextField, EmailField, ImageField, BooleanField, SingleFieldList, ReferenceArrayField, ArrayField, ChipField } from "react-admin";
 
-const RolesArrayField = ({ record, source }) => {
-	const roles = record[source];
-	if (!roles || roles.length === 0) {
-			return <div/>;
-	}
-	return (
-			<>
-					{roles.map(role => <Chip label={role} key={role}/>)}
-			</>
-	);
-};
-
 export const UserList = (props) => (
   <List {...props}>
     <Datagrid rowClick="edit">
@@ -23,21 +11,11 @@ export const UserList = (props) => (
       <ImageField source="photoUrl" sx={{ '& img': { maxWidth: 50, maxHeight: 50, objectFit: 'contain' } }} label="Фото"/>
       <EmailField source="emailData.email" label="Эл. почта"/>
       <BooleanField source="telegramData" valueLabelFalse="null" valueLabelTrue="!!telegramData" label="Телеграм"/>
-			<RolesArrayField source="roles" label="Роли"/>
+			<ArrayField source="roles" label="Роли">
+					<SingleFieldList>
+						<ChipField size="small" source="."/>
+					</SingleFieldList>
+			</ArrayField>
     </Datagrid>
   </List>
 );
-
-const TextArrayField = ({ record, source }) => {
-  const array = record[source]
-  if (typeof array === 'undefined' || array === null || array.length === 0) {
-    return <div/>
-  } else {
-    return (
-      <>
-        {array.map(item => <Chip label={item} key={item}/>)}
-      </>
-    )    
-  }
-}
-TextArrayField.defaultProps = { addLabel: true }
