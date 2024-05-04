9e0b152facff48fe4489c722486db5ee1b4b7520
matveyx25@gmail.com <matveyx25@gmail.com>
2024-05-04 16:31:03 +0400

    fix tabs by mui

--- src/components/Admin/User/UserTabs.jsx
+++ src/components/Admin/User/UserTabs.jsx
@@ -1,36 +1,8 @@
-import { Box, Card, Tabs, Typography } from '@mui/material'
+import { Box, Card, Tab, Tabs } from '@mui/material'
 import React from 'react'
-import { Tab, TabbedForm } from 'react-admin'
 import { UserEdit } from './UserEdit'
 import { Requisites } from './Requisites';
-
-
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 
 export const UserTabs = () => {
 	const [value, setValue] = React.useState(0);
@@ -41,18 +13,20 @@ export const UserTabs = () => {
 
 	return (
 		<Card>
-			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-				<Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-					<Tab label="Общее" {...a11yProps(0)} />
-					<Tab label="Реквизиты" {...a11yProps(1)} />
-				</Tabs>
-			</Box>
-			<CustomTabPanel value={value} index={0}>
-				<UserEdit/>
-			</CustomTabPanel>
-			<CustomTabPanel value={value} index={1}>
-				<Requisites/>
-			</CustomTabPanel>
+			<TabContext>
+				<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+					<TabList onChange={handleChange} aria-label="basic tabs example">
+						<Tab label="Общее" value={0} />
+						<Tab label="Реквизиты" value={1} />
+					</TabList>
+				</Box>
+				<TabPanel value={0}>
+					<UserEdit/>
+				</TabPanel>
+				<TabPanel value={1}>
+					<Requisites/>
+				</TabPanel>
+			</TabContext>
 		</Card>
 	)
 }
