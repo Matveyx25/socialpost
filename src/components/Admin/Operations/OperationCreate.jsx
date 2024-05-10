da59bdfa1fa301ed16e37f6349b0c7a581db69e2
matveyx25@gmail.com <matveyx25@gmail.com>
2024-05-11 01:01:43 +0500

    create style

--- src/components/Admin/Operations/OperationCreate.jsx
+++ src/components/Admin/Operations/OperationCreate.jsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import * as React from "react";
 import {
   Create,
@@ -9,25 +10,31 @@ import {
 
 export const OperationCreate = () => (
   <Create>
-    <SimpleForm>
-      <TextInput
-        source="userId"
-        validate={[required()]}
-        fullWidth
-        label="ID пользователя"
-      />
-      <SelectInput
-        label="Тип"
-        source="type"
-        choices={[
-          { id: "INCOME", name: "Поступление" },
-          { id: "WITHDRAWAL_SELF_EMPLOYED", name: "Вывод у самозанятого" },
-          { id: "WITHDRAWAL_IE", name: "Вывод у ИП" },
-          { id: "WITHDRAWAL_LEGAL_ENTITY", name: "Вывод у ЮЛ" },
-          { id: "WITHDRAWAL_CRYPTO_WALLET", name: "Вывод с криптокошелька" },
-        ]}
-      />
-      <TextInput source="amount" label="Сумма" validate={[required()]}/>
+    <SimpleForm sx={{ maxWidth: 300 }}>
+			<Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+				<TextInput
+					source="userId"
+					validate={[required()]}
+					fullWidth
+					label="ID пользователя"
+				/>
+			</Box>
+			<Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+				<SelectInput
+					label="Тип"
+					source="type"
+					fullWidth
+					choices={[
+						{ id: "INCOME", name: "Поступление" },
+						{ id: "WITHDRAWAL_SELF_EMPLOYED", name: "Вывод у самозанятого" },
+						{ id: "WITHDRAWAL_IE", name: "Вывод у ИП" },
+						{ id: "WITHDRAWAL_LEGAL_ENTITY", name: "Вывод у ЮЛ" },
+					]}
+				/>
+			</Box>
+			<Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+      	<TextInput source="amount" label="Сумма" validate={[required()]} fullWidth/>
+			</Box>
     </SimpleForm>
   </Create>
 );
