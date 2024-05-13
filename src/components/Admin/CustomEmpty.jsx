f9f4b19f7a7ef2d60a97b429dae5410d05268ff5
matveyx25@gmail.com <matveyx25@gmail.com>
2024-05-13 16:11:16 +0400

    CustomEmpty

--- src/components/Admin/CustomEmpty.jsx
+++ src/components/Admin/CustomEmpty.jsx
@@ -1,12 +1,52 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+import Inbox from '@mui/icons-material/Inbox';
 
 export const CustomEmpty = ({message}) => {
-	return (
-		<Box textAlign="center" m={1}>
-			<Typography variant="h4" paragraph>
-					{message}
-			</Typography>
-	</Box>
-	)
-}
+
+    return (
+        <Root>
+            <div className={EmptyClasses.message}>
+                <Inbox className={EmptyClasses.icon} />
+                <Typography variant="h4" paragraph>
+                    {message}
+                </Typography>
+            </div>
+        </Root>
+    );
+};
+
+const PREFIX = 'RaEmpty';
+
+export const EmptyClasses = {
+    message: `${PREFIX}-message`,
+    icon: `${PREFIX}-icon`,
+    toolbar: `${PREFIX}-toolbar`,
+};
+
+const Root = styled('span', {
+    name: PREFIX,
+    overridesResolver: (props, styles) => styles.root,
+})(({ theme }) => ({
+    flex: 1,
+    [`& .${EmptyClasses.message}`]: {
+        textAlign: 'center',
+        opacity: theme.palette.mode === 'light' ? 0.5 : 0.8,
+        margin: '0 1em',
+        color:
+            theme.palette.mode === 'light'
+                ? 'inherit'
+                : theme.palette.text.primary,
+    },
+
+    [`& .${EmptyClasses.icon}`]: {
+        width: '9em',
+        height: '9em',
+    },
+
+    [`& .${EmptyClasses.toolbar}`]: {
+        textAlign: 'center',
+        marginTop: '2em',
+    },
+}));
