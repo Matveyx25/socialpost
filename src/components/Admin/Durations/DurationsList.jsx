ce5243391f8e26390b084ba7c782d43c25f20ebf
Matveyx25 <matveyx25@gmail.com>
2025-06-28 11:28:21 +0400

    remove empty page

--- src/components/Admin/Durations/DurationsList.jsx
+++ src/components/Admin/Durations/DurationsList.jsx
@@ -5,7 +5,8 @@ import { PostPagination } from "../PostPagination";
 
 
 export const DurationsList = (props) => (
-  <List {...props} exporter={false} empty={<CustomEmpty createBtn={'Создать тип'} message={'Типов публикации нет'}/>} pagination={<PostPagination/>}>
+  <List {...props} exporter={false} pagination={<PostPagination/>}>
+  {/* <List {...props} exporter={false} empty={<CustomEmpty createBtn={'Создать тип'} message={'Типов публикации нет'}/>} pagination={<PostPagination/>}> */}
     <Datagrid  bulkActionButtons={false} rowClick="edit">
       <TextField source="id" />
       <TextField source="publishHours" label="Время публикации"/>
