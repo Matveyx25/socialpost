ebfec465a60a762d69b33844b5b5c8b9b56c93ac
Matveyx25 <matveyx25@gmail.com>
2026-02-18 13:08:00 +0400

    Tags label

--- src/components/Admin/AdminPanel.jsx
+++ src/components/Admin/AdminPanel.jsx
@@ -89,7 +89,7 @@ const AdminPanel = () => (
   <Admin dataProvider={dataProvider} basename="/admin" layout={MyLayout}>
 		<Resource name="users" list={UserList} edit={UserTabs} options={{ label: 'Пользователи' }} icon={IconUser}/>
     <Resource name="channels" list={ChannelsList} edit={ChannelEdit} options={{ label: 'Каналы' }} icon={IconSpeakerphone}/>
-    <Resource name="channels/tags" list={TagsShow} options={{ label: 'Каналы' }} icon={IconTag}/>
+    <Resource name="channels/tags" list={TagsShow} options={{ label: 'Теги' }} icon={IconTag}/>
     <Resource name="balance_operations" list={OperationsList} create={OperationCreate} edit={OperationEdit} options={{ label: 'Финансовые операции' }} icon={IconMoneybag}/>
     <Resource name="users/agreements" list={DocumentsList} show={DocumentsShow} options={{ label: 'Договоры' }} icon={IconFileDots}/>
     <Resource name="campaigns" list={CampaignsList} edit={CampaignsEdit} options={{ label: 'Рекламные кампании' }} icon={IconBadgeAd}/>
